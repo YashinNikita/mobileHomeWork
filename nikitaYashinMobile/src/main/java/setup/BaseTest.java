f3ffedf846ba8fa6ed13b387d3f8532540cf046e
Nikita Iashin <Nikita_Iashin@epam.com>
2020-09-26 21:43:51 +0300

    demo

--- nikitaYashinMobile/src/main/java/setup/BaseTest.java
+++ nikitaYashinMobile/src/main/java/setup/BaseTest.java
@@ -16,20 +16,31 @@ public class BaseTest implements IDriver {
     IPageObject po;
 
     @Override
-    public AppiumDriver getDriver() { return appiumDriver; }
+    public AppiumDriver getDriver() {
+        return appiumDriver;
+    }
 
     public IPageObject getPo() {
         return po;
     }
 
-    @Parameters({"platformName","appType","deviceName","browserName","app"})
+    @Parameters({"platformName", "appType", "deviceName", "udid", "browserName", "app", "appPackage", "appActivity", "bundleId"})
     @BeforeSuite(alwaysRun = true)
-    public void setUp(String platformName, String appType, String deviceName, @Optional("") String browserName, @Optional("") String app) throws Exception {
-        System.out.println("Before: app type - "+appType);
-        setAppiumDriver(platformName, deviceName, browserName, app);
-        setPageObject(appType, appiumDriver);
 
+    public void setUp(String platformName, String appType,
+                      @Optional("") String deviceName,
+                      @Optional("") String udid,
+                      @Optional("") String browserName,
+                      @Optional("") String app,
+                      @Optional("") String appPackage,
+                      @Optional("") String appActivity,
+                      @Optional("") String bundleId
+    ) throws Exception {
+        System.out.println("Before: app type - " + appType);
+        setAppiumDriver(platformName, deviceName, udid, browserName, app, appPackage, appActivity, bundleId);
+        setPageObject(appType, appiumDriver);
     }
+
 
     @AfterSuite(alwaysRun = true)
     public void tearDown() throws Exception {
@@ -37,30 +48,34 @@ public class BaseTest implements IDriver {
         appiumDriver.closeApp();
     }
 
-    private void setAppiumDriver(String platformName, String deviceName, String browserName, String app){
+    private void setAppiumDriver(String platformName, String deviceName, String udid, String browserName, String app,
+                                 String appPackage, String appActivity, String bundleId) {
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        //mandatory Android capabilities
-        capabilities.setCapability("platformName",platformName);
-        capabilities.setCapability("deviceName",deviceName);
-
-        if(app.endsWith(".apk")) capabilities.setCapability("app", (new File(app)).getAbsolutePath());
-
+        // mandatory Android capabilities
+        capabilities.setCapability("platformName", platformName);
+        capabilities.setCapability("deviceName", deviceName);
+        capabilities.setCapability("udid", udid);
+        if (app.endsWith(".apk")) capabilities.setCapability("app", (new File(app)).getAbsolutePath());
         capabilities.setCapability("browserName", browserName);
-        capabilities.setCapability("chromedriverDisableBuildCheck","true");
-
+        capabilities.setCapability("chromedriverDisableBuildCheck", "true");
+        // Capabilities for test of Android native app on EPAM Mobile Cloud
+        capabilities.setCapability("appPackage", appPackage);
+        capabilities.setCapability("appActivity", appActivity);
+        // Capabilities for test of iOS native app on EPAM Mobile Cloud
+        capabilities.setCapability("bundleId", bundleId);
+        //if(platformName.equals("iOS")) capabilities.setCapability("automationName","XCUITest");
         try {
             appiumDriver = new AppiumDriver(new URL(System.getProperty("ts.appium")), capabilities);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-
         // Timeouts tuning
         appiumDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
     }
 
     private void setPageObject(String appType, AppiumDriver appiumDriver) throws Exception {
         po = new PageObject(appType, appiumDriver);
+
     }
 
 
--- nikitaYashinMobile/src/main/java/setup/IPageObject.java
+++ nikitaYashinMobile/src/main/java/setup/IPageObject.java
@@ -3,7 +3,12 @@ package setup;
 import org.openqa.selenium.WebElement;
 
 public interface IPageObject {
+    WebElement getWelement(String weName)  throws NoSuchFieldException, IllegalAccessException, InstantiationException;
+    WebElement getBaseNativePageObject(String weName) throws NoSuchFieldException, IllegalAccessException, InstantiationException;
+    WebElement getBudgetActivityNativePageObject(String weName) throws NoSuchFieldException, IllegalAccessException, InstantiationException;
+    WebElement getRegistrationNativePageObject(String weName) throws NoSuchFieldException, IllegalAccessException, InstantiationException;
+    WebElement getGoogleHomePageObject(String weName) throws NoSuchFieldException, IllegalAccessException, InstantiationException;
 
-    WebElement getWelement(String weName) throws NoSuchFieldException, IllegalAccessException, InstantiationException;
+
 
 }
